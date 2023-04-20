7b722e848651d967b22711c4d771590e4c27285c
SimoneRicco <smnrcc0@gmail.com>
2023-04-20 17:21:20 +0200

    modified

--- assets/script.js
+++ assets/script.js
@@ -9,23 +9,44 @@ document.querySelector("#send").addEventListener("click", function () {
 });
 
 document.querySelector(".bottone").addEventListener("click", function () {
-    document.querySelector(".inputs").innerHTML = `<input type="text" placeholder="inserisci un numero">
+    document.querySelector(".inputs").innerHTML = `<input type="number" placeholder="inserisci un numero" max="5" min="1">
     <input type="submit" id="btn-2">`;
-});
-document.querySelector(".btn-2").addEventListener("click", function () {
-    const pcNumber = Math.floor(Math.random() * 5) + 1;
-    const evenOdd = document.querySelector(".bottone").value;
-    const myNumber = document.querySelector("#number");
-    document.querySelector(".computer-number").innerHTML = "Il pc ha scelto il numero: "+pcNumber;
-    const sum = pcNumber + myNumber;
-    if (evenOdd == pari) {
-        if (sum % 2 == 0) {
-            document.querySelector(".output2").innerHTML = "pari";
+    document.querySelector("#btn-2").addEventListener("click", function () {
+        const pcNumber = Math.floor(Math.random() * 5) + 1;
+        const evenOdd = document.querySelector(".bottone").value;
+        const myNumber = document.querySelector("#number");
+        document.querySelector(".computer-number").innerHTML = "Il pc ha scelto il numero: "+pcNumber;
+        const sum = pcNumber + myNumber;
+        if (evenOdd == "pari") {
+            if (sum % 2 == 0) {
+                document.querySelector(".output2").innerHTML = "Hai vinto";
+            }else{
+                document.querySelector(".output2").innerHTML = "Hai perso";
+            }
         }else{
-            document.querySelector(".output2").innerHTML = "dispari";
+            if (sum % 2 == 0) {
+                document.querySelector(".output2").innerHTML = "Hai perso";
+            }else{
+                document.querySelector(".output2").innerHTML = "Hai vinto";
+            }
         }
-    }
+    });
 });
+// document.querySelector("#btn-2").addEventListener("click", function () {
+//     alert("ciao");
+//     const pcNumber = Math.floor(Math.random() * 5) + 1;
+//     const evenOdd = document.querySelector(".bottone").value;
+//     const myNumber = document.querySelector("#number");
+//     document.querySelector(".computer-number").innerHTML = "Il pc ha scelto il numero: "+pcNumber;
+//     const sum = pcNumber + myNumber;
+//     if (evenOdd == pari) {
+//         if (sum % 2 == 0) {
+//             document.querySelector(".output2").innerHTML = "pari";
+//         }else{
+//             document.querySelector(".output2").innerHTML = "dispari";
+//         }
+//     }
+// });
 
 function checkPalindrom(w) {
     let isPalindrom = true;
