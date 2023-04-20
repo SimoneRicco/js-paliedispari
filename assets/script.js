ae36a9e28bdae73c08dbf736bed0487ebd28c4da
SimoneRicco <smnrcc0@gmail.com>
2023-04-20 17:05:24 +0200

    dispari non funziona

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+document.querySelector("#send").addEventListener("click", function () {
+    const word = document.querySelector("#word").value;
+    const out = document.querySelector(".output1");
+    if (checkPalindrom(word)) {
+        out.innerHTML = "è palindromo";
+    }else{
+        out.innerHTML = "non è palindromo";
+    }
+});
+
+document.querySelector(".bottone").addEventListener("click", function () {
+    document.querySelector(".inputs").innerHTML = `<input type="text" placeholder="inserisci un numero">
+    <input type="submit" id="btn-2">`;
+});
+document.querySelector(".btn-2").addEventListener("click", function () {
+    const pcNumber = Math.floor(Math.random() * 5) + 1;
+    const evenOdd = document.querySelector(".bottone").value;
+    const myNumber = document.querySelector("#number");
+    document.querySelector(".computer-number").innerHTML = "Il pc ha scelto il numero: "+pcNumber;
+    const sum = pcNumber + myNumber;
+    if (evenOdd == pari) {
+        if (sum % 2 == 0) {
+            document.querySelector(".output2").innerHTML = "pari";
+        }else{
+            document.querySelector(".output2").innerHTML = "dispari";
+        }
+    }
+});
+
+function checkPalindrom(w) {
+    let isPalindrom = true;
+    for (let i = 0; i < w.length && isPalindrom; i++) {
+        if (w[i] != w[w.length - i - 1]) {
+            isPalindrom = false
+        }
+    }
+    return isPalindrom;
+}
